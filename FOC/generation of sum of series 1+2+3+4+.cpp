08217f3cbafdd0fed41509fdd6cf769363174132
sravankumardoddavaram <124283026+sravankumardoddavaram@users.noreply.github.com>
2023-02-03 10:11:37 +0530

    Create generation of sum of series 1+2+3+4+.cpp

---
+++ FOC/generation of sum of series 1+2+3+4+.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,sum=0;
+	printf("\nEnter a number:");
+	scanf("%d", &n);
+	for(i=1;i<=n;++i)
+	{
+		sum=sum+i;
+	}
+	printf("sum= %d",sum);
+	return 0;
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
