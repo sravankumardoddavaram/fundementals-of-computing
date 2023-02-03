d3abc26c71be7a0f351503e1dcc2eb8a36a9464d
sravankumardoddavaram <124283026+sravankumardoddavaram@users.noreply.github.com>
2023-02-03 10:29:33 +0530

    Create summing up squares of odd numbers.cpp

---
+++ FOC/summing up squares of odd numbers.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a=1,sum=0,n;
+	printf("\nEnter a number:");
+	scanf("%d,&n");
+	while (a<=n)
+	{
+		sum=sum+(a*a);
+		a=a+2;
+	}
+	printf("\n sum= %d",sum);
+	return 0;
+
+}
